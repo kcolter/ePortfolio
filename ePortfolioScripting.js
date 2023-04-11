eec4015f02d6511618e9f692ee72e063699a09dd
kcolter <62590150+kcolter@users.noreply.github.com>
2023-04-11 06:36:32 -0300

    deleted old css, opting to start from scratch

--- ePortfolioScripting.js
+++ ePortfolioScripting.js
@@ -1,5 +1,7 @@
 //Author Keegan Colter
 
+//researched from https://dev.to/ljcdev/easy-hamburger-menu-with-js-2do0#:~:text=Begin%20by%20adding%20a%20basic%20menu%20with%20a,and%20alternate%20which%20icon%20to%20show%20with%20Javascript.
+
 function navMenuResponsive() {
     var x = document.getElementById("directory");
     if (x.className === "directory") {
