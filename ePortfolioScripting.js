4621ef3fd9473193484c81fe4acc079d8b66c20b
kcolter <kcolter3@gmail.com>
2023-04-13 11:26:55 -0300

    coded consts for elements in js

--- ePortfolioScripting.js
+++ ePortfolioScripting.js
@@ -1,1 +1,10 @@
 //Author Keegan Colter
+
+//elements
+const menu = document.getElementById("menu");
+const menuItems = document.getElementById("menuItem");
+const hamburgerButton = document.getElementById("hamburgerButton");
+
+//elements for the close and open icons, will be added after functionality complete
+const closeIcon = null;
+const menuIcon = null;
